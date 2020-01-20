d85a4d6db0c4616aa4bebffd7b816152150af7e1
Megalopsychic <mfkourie@gmail.com>
2020-01-20 20:26:44 +0000

    completed hello, temperature, and caluculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
-#write your code here
+def add(a, b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b
+end
+
+def sum(array)
+    array.sum
+end
+
+def multiply(array)
+    i = 1
+    result = array[0]
+    while i < array.length
+        result = result * array[i]
+        i += 1
+    end
+    result
+end
+
+def power(a, b)
+    a**b
+end
+
+def factorial(n)
+    product = 1
+    if n == 0
+        return 1
+    else
+        while n > 0
+            product = product * n
+            n -=1
+        end
+    end
+    product
+end  
